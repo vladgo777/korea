8683031feb2d2c94193b5434ca860639f0a86601
vkudok <vladkutepov1@gmail.com>
2021-03-06 22:06:35 +0300

    mobile menu

--- build/js/script.js
+++ build/js/script.js
@@ -6,3 +6,23 @@ function SmallText(i){
 $('.text-1').on('click',function(){
     SmallText(1);
 });
+
+//open burger
+$('.burger').click(function(){
+    $(this).toggleClass('open');
+    $('.mobileHeader__li').toggleClass('open');
+    });
+
+//background header
+var menu = $(".header");
+var menu1 = $(".mobileHeader");
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    if ( top >= 100 ) {
+      menu.addClass('background');
+      menu1.addClass('background');
+    } else if ( top <= 200 ) {
+        menu.removeClass('background');
+        menu1.removeClass('background');
+    }
+  });
