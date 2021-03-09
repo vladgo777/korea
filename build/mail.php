5f8702c092009e675c612ad1806c668e6aaf0861
vkudok <vladkutepov1@gmail.com>
2021-03-09 21:36:07 +0300

    готово

--- build/mail.php
+++ build/mail.php
@@ -1,10 +1,19 @@
 <?php
 
+$token = "1604909264:AAHcwA5VAvhBkGR2kQoMVk5XBU84vJx3i38";
+
+$chat_id = "-549352411";
+
 if (isset($_POST["form_name"]) && isset($_POST["form_phone"]) ) { 
 
 $name = $_POST['form_name'];
 $phone = $_POST['form_phone']; 
 // $email = $_POST['email'];
+
+$arr = array(
+    'Имя:' => $name,
+    'Телефон:' => $phone
+);
 
 //Теперь давайте настроим куда отправляем и откуда
 
@@ -20,8 +29,12 @@ $mes = "
 ";
 
 //Всё, теперь можно отправлять письмо на почту
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToMail = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
 
-$send = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
+if(($sendToTelegram == true) &&($sendToMail == true)){
+    alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
+}
 
 }
 
